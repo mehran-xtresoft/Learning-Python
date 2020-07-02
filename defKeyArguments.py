c996ec5d28f3021b3a4ef10736699dd3bfa79668
Mehran <mehran@xtresoft.com>
2020-07-02 13:44:19 +0500

    defKeyArgument

---
+++ defKeyArguments.py
@@ -0,0 +1,13 @@
+def parrot(voltage, state='a stiff', action='voom', type='Norwegian Blue'):
+    print('-- This parrot wouldn\'t', action, end=' ')
+    print('if you put", voltage, "volts through it.')
+    print('-- Lovely plumage, the', type)
+    print('-- It\'s', state, '!')
+
+
+parrot(1000)
+parrot(voltage=1000)
+parrot(voltage=1000000, action='VOOOOOM')
+parrot(action='VOOOOOM', voltage=1000000)
+parrot('a million', 'bereft of life', 'jump')
+parrot('a thousand', state='pushing up the daisies')
